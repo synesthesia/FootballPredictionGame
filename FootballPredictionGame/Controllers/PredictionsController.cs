20474c6e1f88baf9e7e56cfd4e67e472dc166172
GeorgeMB <george.burgess@ssatuk.co.uk>
2018-06-03 13:34:52 +0100

    Predictions Editable Grid - Initial

---
+++ FootballPredictionGame/Controllers/PredictionsController.cs
@@ -0,0 +1,33 @@
+﻿using FootballPredictionGame.DAL;
+using FootballPredictionGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootballPredictionGame.Controllers
+{
+    public class PredictionsController : Controller
+    {
+        private PredictionContext db = new PredictionContext();
+
+        // GET: Predictions
+        public ActionResult Index()
+        {
+            var predictions = db.Predictions.Where(p => p.PredictorId == 3).ToList();
+
+            return View(predictions);
+        }
+
+        public JsonResult ChangeUser(Prediction model)
+        {
+            // Update model to your db
+            string message = "Success";
+            return Json(message, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+    }
+}
